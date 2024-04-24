a265cee3bc2423ccdbd92dd77fccfaea378840f9
Victor <emmyvic98@gmail.com>
2024-04-24 18:29:09 +0100

    cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,8 @@ import { getData } from "./actions";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const token = cookies().get("access_token");
   const profileRequest = await getData("/user/myprofile");
-  console.log("Token value == ", token?.value);
-  console.log("Profile request == ", profileRequest);
+
   if(profileRequest.statusCode === 200 && request.nextUrl.pathname.startsWith('/auth')){
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
